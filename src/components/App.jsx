ba1449fd2f6646b5976ee6acefdab55e593eeaa3
Ariadna Ruiz <arsarbite@gmail.com>
2023-10-20 18:30:28 +0200

    initial

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+
+import reactLogo from '../images/react.svg'
+import '../styles/App.scss'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+ 
+
+  return (
+    <>
+  <div>
+  <h1>Owens Wilson's "wow"</h1>
+  </div>
+  <div>
+    <h2>Listado de escenas de Owen Wilson</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+   </>
+  )
+}
+
+
+export default App
