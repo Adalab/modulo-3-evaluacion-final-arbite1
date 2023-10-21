1ac22088d02ff6900b74ab925827d436cb0db698
Ariadna Ruiz <arsarbite@gmail.com>
2023-10-21 17:45:32 +0200

    add ls

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import '../styles/App.scss'
 import callToApi from './services/api';
-//import ls from '../services/localStorage';
+import ls from './services/localStorage';
 import { useEffect, useState } from 'react';
 import MovieSceneList from './MovieSceneList';
 //import MovieSceneItem from './MovieSceneItem';
 
 function App() {
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(ls.get("movies", []));
 
   useEffect(() => {
+    if(ls.get("movies", null) === null){ 
     callToApi().then((cleanData) => {
       setData(cleanData);
+      ls.set("movies", cleanData);
     });
+  }
   }, []);
 
   
